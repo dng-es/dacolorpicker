db3658ded3aa89221a09d8bba8eca1a6e6ae42f8
David Noguera Gutierrez <nogueradavid@hotmail.com>
2013-10-20 02:15:04 +0200

    version 1.0.1

---
+++ js/jquery.dacolorpicker.js
@@ -0,0 +1,106 @@
+/*!
+ * Da colorpicker
+ *
+ * @name     dacolorpicker
+ * @param    paletteShow      html element where color is sent
+ * @author   David Noguera Gutierrez
+ * @example  $("#trigger1").dacolorpicker({paletteShow: true,paletteType: "palettePattern1"});
+ */
+(function($) {
+ jQuery.fn.dacolorpicker = function(options) {
+	//Default options
+	var configuration = {
+	  paletteShow: true,
+	  paletteType: "palettePattern1"
+	}
+	jQuery.extend(configuration, options);
+
+	this.each(function(){
+		var e_trigger, idPalette, idInfo, colorsPalette, daPicker,daPickerImg;
+
+		e_trigger = $(this);		
+		idPalette = "#" + $(this).attr("name")+"_paleta";
+		idInfo = $(this).attr("name")+"_paleta_info";
+		colorsPalette = choosePalette();
+		daPicker = $('<div id="'+idPalette+'" class="dacolor-picker">'+colorsPalette+'<div class="cpicker-info" id="'+idInfo+'">Color: </div></div>');
+		daPicker.appendTo("body");
+		if (configuration.paletteShow){
+			daPickerImg = $('<img src="images/cpicker.png" class="cpicker">');
+			daPickerImg.insertAfter(e_trigger);
+			daPickerImg.click(function() {
+				showPalette($(this));
+			});
+		}
+
+		/*e_trigger click event*/
+		e_trigger.click(function(){
+			showPalette($(this));
+		});
+
+		/*e_trigger mouseleave event */
+		daPicker.mouseleave(function(){
+			daPicker.fadeOut(200);			
+		});
+
+		/*Color click event*/
+		daPicker.delegate("span", "click", function() {
+			var color = $(this).attr("title");
+			e_trigger.attr('value',color);
+			daPicker.fadeOut(200);
+		});
+
+		/*Color mouseover event*/
+		daPicker.delegate("span", "mouseover", function() {
+			var color = $(this).attr("title"),
+				infoLabel = "#" + idInfo;
+			$(infoLabel).html('Color: '+color);
+		});
+
+		function showPalette(e){
+			var posx, posy;
+			$(".dacolor-picker").hide();
+			posx = configuration.paletteShow ? (e_trigger.outerWidth() + daPickerImg.outerWidth() + 10) : (e_trigger.outerWidth()+10);
+			posy = e.offset().top;		
+			daPicker.css({"position" : "absolute","top": posy,"left": posx});
+			daPicker.fadeIn(500);
+		}
+
+		/*Color Palettes*/
+		function choosePalette(){
+			return createPalette(configuration.paletteType=="palettePattern2" ? palettePattern2() : palettePattern1());
+		}	  
+	});
+	return this;
+
+   	/*General functions*/
+	function createPalette(fn){
+		var output = "", i, j, k, new_color,
+			colors = fn;
+		for (i=0;i<colors.r.length;i++){
+			for (j=0;j<colors.g.length;j++) {
+			   for (k=0;k<colors.b.length;k++) {
+				  new_color = "#" + colors.r[i] + colors.g[j] + colors.b[k];
+				  output+='<span class="color-pick" style="background:' + new_color + '" title="'+new_color+'"></span>';
+			   }
+			}
+		}
+		return output; 	  	
+	}
+
+	function palettePattern1(){
+		var colors =new Array(3);
+		colors['r'] = new Array("00","33","66","99","CC","FF");
+		colors['g'] = new Array("00","33","66","99","CC","FF");
+		colors['b'] = new Array("00","33","66","99","CC","FF");
+		return colors; 
+	}
+
+	function palettePattern2(){
+		var colors =new Array(3);	
+		colors['r'] = new Array("C1","B4","A7","9A","8E","00");
+		colors['g'] = new Array("C1","00","22","44","66","88");
+		colors['b'] = new Array("C1","11","33","55","77","99");
+		return colors;   
+	}   
+};
+})(jQuery);
